396610d1680394fcd3133266075d9cd260ca1c03
Ekaterina Gnedysheva <gnedyshevaeg@gmail.com>
2025-12-29 15:50:16 +0100

    4hw

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,16 @@
-// type InputPropsType = {
-// 	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-// 	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-// };
-//
-// export const Input = (props: InputPropsType) => {
-// 	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-// 		// НУЖНО ДОПИСАТЬ
-// 	};
-//
-// 	// return (
-// 	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-// 	// );
-// };
+import type {ChangeEvent} from "react";
+
+type InputPropsType = {
+	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText: (text: string)=>void // НУЖНО ПРОТИПИЗИРОВАТЬ
+};
+
+export const Input = (props: InputPropsType) => {
+	const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        props.setCurrentText(event.currentTarget.value)
+	};
+
+	return (
+	  <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+	);
+};
